adcbf010711f981cbc7c6c81b49ece2bfd574dd9
devguy6 <121516494+devguy6@users.noreply.github.com>
2026-04-01 15:28:42 -0700

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ACTKumon from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ACTKumon />
+  </React.StrictMode>
+);
